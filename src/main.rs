4398ab73a20048998159c25ac9912db81040ed36
Andrii V <7934862+garrich@users.noreply.github.com>
2024-01-28 10:56:52 +0100

    Add command line arguments processing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use md5;
+use std::env;
 use std::{fs::File, io::{BufRead, BufReader}};
 
 fn main() {
-    println!("Hello, world!");
-    func("c:\\Users\\garrich\\Downloads\\windirstat1_1_2_setup.exe")
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() > 1 {
+        let file_path = &args[1];
+        calc_hash(file_path)
+    }else {
+        let file_path = &args[0];
+        println!("Please provide path to a file for md5 hash calculation. \nAs no path provided - self file is used\n{}", file_path)
+    }
+    
 }
 
-fn func(file_path: &str) {
+fn calc_hash(file_path: &str) {
     let f = File::open(file_path).unwrap();
     // Find the length of the file
     let len = f.metadata().unwrap().len();
@@ -27,6 +36,7 @@ fn func(file_path: &str) {
         let part_len = part.len();
         buf.consume(part_len);
     }
-    let digest = context.compute();
+
+    let digest  = context.compute();
     println!("{:x}\t{}", digest, file_path);
 }
